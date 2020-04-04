7c81d77fa84aecd9005d74d4c19bf092314a00b6
Mikhail <m-gasanov@ya.ru>
2020-04-04 15:49:24 +0500

    Увеличение товаров в крорзине

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const HOST ='http://localhost:8080'
 export default new Vuex.Store({
   state: {
     products:[],
@@ -19,7 +20,7 @@ export default new Vuex.Store({
   },
   actions: {
     GET_PRODUCTS_FROM_API({commit}) {
-      fetch('http://localhost:3000/products')
+      fetch(HOST + '/products')
       .then(response => {
         return response.json();
       })
@@ -31,106 +32,100 @@ export default new Vuex.Store({
         
       })
     },
-    
-    ADD_TO_CART({state}, product) {
-      console.log(product);
-      let isProduct = false;
-      this.state.cart.forEach(el => {
-        if(el.id == product.id){
-          el.quantity++;
-          isProduct = true;
-        }
-
+    GET_CART_FROM_API({commit}) {
+      fetch(HOST + '/cart')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        commit('SET_CART', data)
+      })
+      .catch(err => {
+        console.log(err);
         
       })
-      if(!isProduct){
-        product.quantity = 1
-        this.state.cart.push(product)
-      }
-      
-
-        /* if(!state.cart.length){
-          product.quantity = 1;
-                       
-            fetch('http://localhost:3000/cart', {
-              method: 'post',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(product)
-            })
-            .then(response => {
-              
-              return response.json();
-            })
-            .then(data => {
-              dispatch('GET_CART_FROM_API')
-              
-            })
-            .catch(err => {
-              console.log(err);
-              
-            })
-        }else{
-          state.cart.forEach(element => {
-            if (element.id == product.id){
-
-              product.quantity++;
-              fetch('http://localhost:3000/cart/' + product.id, {
-                method: 'patch',
-                headers: {
-                  
-                'Content-Type': 'application/json',
-                
-                  
-                },
-                
-                
-                body: JSON.stringify({quantity: 1}),
-                mode: 'cors'
-              })
-              .then(response => {
-                console.log(response.status);
-                return response.json();
-              })
-              .then(data => {
-                dispatch('GET_CART_FROM_API')
-                
-              })
-              .catch(err => {
-                console.log(err);
-                
-              })
-              
-            }else{
-              /* product.quantity = 1;
-              
-              
-              fetch('http://localhost:3000/cart', {
-                method: 'post',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(product)
-              })
-              .then(response => {
-                console.log(response.status);
-                return response.json();
-              })
-              .then(data => {
-                dispatch('GET_CART_FROM_API')
-                
-              })
-              .catch(err => {
-                console.log(err);
-                
-              })
-              
-            } 
-          }); 
-        }
+    },
+    
+    ADD_TO_CART({dispatch, state}, product) {
+      if(!state.cart.length){
+      product.quantity = 1;
+                    
+        fetch(HOST + '/cart', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(product)
+        })
+        .then(response => {
+          
+          return response.json();
+        })
+        .then(data => {
+          dispatch('GET_CART_FROM_API')
+          
+        })
+        .catch(err => {
+          console.log(err);
+          
+        })
+      }else{
+        let isProduct = false;
+        this.state.cart.forEach(el => {
+          if(el.id == product.id){
+            isProduct = true;
+          }
+        })
         
-      */
+        if (isProduct){
+          isProduct = false;
+          product.quantity++;
+          console.log(product)
+          let change = {
+            quantity: product.quantity,
+          }
+          fetch(HOST + '/cart/' + product.id, {
+            method: 'PATCH',
+            
+            headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify(change),
+          })
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            dispatch('GET_CART_FROM_API')
+          })
+          .catch(err => {
+            console.log(err);
+          })
+          
+        }else{
+          product.quantity = 1;
+          fetch(HOST + '/cart', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+          })
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            dispatch('GET_CART_FROM_API')
+            
+          })
+          .catch(err => {
+            console.log(err);
+            
+          })
+          
+        } 
+      
+      }
     },
   },
   
